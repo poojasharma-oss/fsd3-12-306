a66b308e84feb5ddd9c5213692658b05bffcc06e
poojasharma-oss <poojapintoosharma@gmail.com>
2026-09-17 11:55:50 +0530

    lab4 setup

--- lab4/prg7.js
+++ lab4/prg7.js
@@ -5,7 +5,7 @@ const server = http.createServer((req, res) => {
   if (req.url === "/api/users" && req.method === "GET") {
     res.end(JSON.stringify(getUsers()));
   } 
-  
+   
   else if (req.url === "/api/users" && req.method === "POST") {
     res.end(JSON.stringify({ msg: "add user" }));
   } 
